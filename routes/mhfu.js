0af4943b698e85dc9e6dc3fbe5a6a1539b51bd50
unknown <RashaanWashington@protonmail.com>
2022-01-02 00:50:41 -0500

    timed out handling tests

--- routes/mhfu.js
+++ routes/mhfu.js
@@ -79,6 +79,7 @@ router.get('/smallmonsters', async (req, res) => {
         .then( () => scrapper.getFurtherInfo(monsters))
         .catch((err) => console.log(err))
 
+    res.status(200).send("working...")
     res.json(monsters)
     console.log("DING!! :D")
     
