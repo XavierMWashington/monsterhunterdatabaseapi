c7c332cf7efa604bd4f2ddc0f28b00631a8ff818
unknown <RashaanWashington@protonmail.com>
2022-01-07 00:48:40 -0500

    testing

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express()
 global.gatheredNames = []
 global.activated = false
 
+const url = require("url-parse")
+
 let redisURL = new url(process.env.REDISCLOUD_URL, {no_ready_check: true}) || 8000
 console.log("Reddis running on this url: " + redisURL)
 
