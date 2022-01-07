89ec83f8a0656ca64f253885184b54751112aded
unknown <RashaanWashington@protonmail.com>
2022-01-07 09:43:57 -0500

    testing

--- index.js
+++ index.js
@@ -9,11 +9,10 @@ const app = express()
 global.gatheredNames = []
 global.activated = false
 
-
 const redis = require('redis')
 const url = require("url-parse")
 
-const redisURL = 8000//new url(process.env.REDISCLOUD_URL, {no_ready_check: true}) || 8000
+const redisURL = new url(process.env.REDISCLOUD_URL, {no_ready_check: true}) || 8000
 console.log("Reddis running on this url: " + redisURL)
 
 const redisClient = redis.createClient(redisURL)
--- redisHandler.js
+++ redisHandler.js
@@ -2,15 +2,15 @@
 //Redis Handling
 //*/
 
-const redis = require('redis')
-const url = require("url-parse")
+// const redis = require('redis')
+// const url = require("url-parse")
 
-const redisURL = new url(process.env.REDISCLOUD_URL, {no_ready_check: true}) || 8000
-console.log("Reddis running on this url: " + redisURL)
+// const redisURL = new url(process.env.REDISCLOUD_URL, {no_ready_check: true}) || 8000
+// console.log("Reddis running on this url: " + redisURL)
 
-const redisClient = redis.createClient(redisURL)
+// const redisClient = redis.createClient(redisURL)
 
-redisClient.connect()
+// redisClient.connect()
 
 
-exports.redisClient = redisClient
+// exports.redisClient = redisClient
