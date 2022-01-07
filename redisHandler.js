210f017550a3868182d863653dbf2af7e4500895
unknown <RashaanWashington@protonmail.com>
2022-01-07 12:57:50 -0500

    testing

--- redisHandler.js
+++ redisHandler.js
@@ -4,7 +4,7 @@ const url = require("url-parse")
 let redisClient
 if(process.env.REDIS_URL){
     let redisURL = process.env.REDIS_URL
-    redisClient = redis.createClient(redisURL, process.env.PORT)
+    redisClient = redis.createClient({url: redisURL})
     redisClient.connect().catch(err => console.log(err))
     console.log("Redis is connected to heroku\nurl:" + redisURL)
 } else {
